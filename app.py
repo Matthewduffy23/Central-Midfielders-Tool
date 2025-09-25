5bf96da16a96469634e4452f77878b88b34b670a
Matthewduffy23 <matthewduffy2000@gmail.com>
2025-09-25 12:50:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -895,7 +895,7 @@ else:
     # --------- layout / padding knobs ---------
     NAME_X   = 0.045   # more breathing room on the left
     META_X   = 0.045
-    CHIP_X0  = 0.06   # chips/roles start x
+    CHIP_X0  = 0.055   # chips/roles start x
     GUTTER_PAD  = 0.006
 
     # ----------------- helpers -----------------
